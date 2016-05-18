bf7aefcad17e7b3e997e32a89d6e223291c9f4a8
Alberto <alberto9.24.93@gmail.com>
2016-05-18 18:06:26 -0500

    Models created.

--- app/src/main/java/com/innovationtechnology/app/hotelturismoapp/MainActivity.java
+++ app/src/main/java/com/innovationtechnology/app/hotelturismoapp/MainActivity.java
@@ -1,12 +1,9 @@
 package com.innovationtechnology.app.hotelturismoapp;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.commit451.nativestackblur.NativeStackBlur;
@@ -27,14 +24,6 @@ public class MainActivity extends AppCompatActivity {
 
         background.setImageBitmap(NativeStackBlur.process(bitmap, 25));
 
-        Button hospedar = (Button) findViewById(R.id.hospedar);
-        assert hospedar != null;
-        hospedar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), HostingActivity.class));
-                overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
-            }
-        });
+
     }
 }
